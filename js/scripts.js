2481b907cec3ba6336e06da49a50275b48b62df7
Emily Sira <esiranjo@gmail.com>
2022-01-16 19:59:35 +0300

    introducing mouseleave event

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,36 @@ $(document).ready(function() {
         $(".clickme1").toggle();
         $("#design-show").toggle();
         $("#design-show").css("textAlign", "center");
-        $("p").on({
-            mouseenter: function(){
-            $(this).css({"font-weight": "bold"});
-            }
+        $( "p" )
+            .on( "mouseenter", function() {
+            $( this ).css({
+            "font-weight": "bolder"
+        });
     })
+            .on( "mouseleave", function() {
+            var styles = {
+            fontWeight: "normal"
+};
+$( this ).css( styles );
+});
     })
-    })
+
     $("#development").click(function(){
         $(".clickme2").toggle();
         $("#development-show").toggle();
         $("#development-show").css("textAlign", "center");
-        $("p").on({
-            mouseenter: function(){
-            $(this).css({"font-weight": "bold"});
-            }
-    })
-    })
+        $("p")
+        .on( "mouseenter", function() {
+            $( this ).css({
+            "font-weight": "bolder"
+        });
+        })
+        .on( "mouseleave", function() {
+            var styles = {
+            fontWeight: "normal"
+};
+$( this ).css( styles );
+});
+})
+})
 // alert('hi')
