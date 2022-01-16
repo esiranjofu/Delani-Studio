3b76d0c94d0c6318870a6b8c271712b960537d46
Emily Sira <esiranjo@gmail.com>
2022-01-16 19:39:08 +0300

    more styling on toggle effect

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 // alert('hi')
 $(document).ready(function() {
     $("#design").click(function(){
-        $(".clickme").toggle();
+        $(".clickme1").toggle();
         $("#design-show").toggle();
         $("#design-show").css("textAlign", "center");
         $("p").on({
@@ -10,6 +10,16 @@ $(document).ready(function() {
             $(this).css({"font-weight": "bold"});
             }
     })
+    })
+    })
+    $("#development").click(function(){
+        $(".clickme2").toggle();
+        $("#development-show").toggle();
+        $("#development-show").css("textAlign", "center");
+        $("p").on({
+            mouseenter: function(){
+            $(this).css({"font-weight": "bold"});
+            }
     })
     })
 // alert('hi')
