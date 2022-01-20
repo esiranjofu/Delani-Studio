905be61609d99b02d7d01e478bd43ac8df352f71
Emily Sira <esiranjo@gmail.com>
2022-01-20 08:53:54 +0300

    if else statement validation

---
+++ function/script.js
@@ -0,0 +1,26 @@
+// $(document).ready(function() {
+//     // confirming if company name is captured
+//     $("#customerinfo form").submit(function(event) {
+// (function checkIndustry(){
+//     var operationIndustry = document.getElementsByName('industry');
+//     var checked=0;
+//     for ( var i = 0; i < operationIndustry.length; i++) {
+//         if(operationIndustry
+//             [i].checked) {
+//             checked++;
+            
+//         }
+//     }
+//     if (checked >0) {
+//         console.log (checked)
+//         return true;
+
+//     }else {
+//       { //industry of service is not selected
+//         alert("Please choose your service industry");
+//         return false
+//     }
+// }
+// })
+//     })
+// })
